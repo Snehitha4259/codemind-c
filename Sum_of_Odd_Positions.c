f541335ab289f48215d550aaf1c2780ea33feb63
Snehitha4259 <147792508+Snehitha4259@users.noreply.github.com>
2023-12-15 11:24:35 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);    int ar[n],sum=0,i;     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0) sum=sum+ar[i];     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+   int ar[n],sum=0,i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0) sum=sum+ar[i];
+    }
+    printf("%d",sum);
+}
